45269614f635bae674082ea98e70b9fd7fa4a898
JaeWooYi <wodn0101@gmail.com>
2022-09-30 09:39:35 +0900

    First

---
+++ index.js
@@ -0,0 +1,27 @@
+var http = require("http");
+var hostName = "127.0.0.1";
+var port = 8070;
+
+const server = http.createServer(function (req, res) {
+  const path = req.url;
+  const method = req.method;
+  if (path === "/products") {
+    if (method === "GET") {
+      res.writeHead(200, { "Content-type": "application/json" });
+      const products = JSON.stringify([
+        {
+          name: "CUP",
+          price: "400$",
+        },
+      ]);
+      res.end(products);
+    } else if (method === "POST") {
+      res.end("Created!");
+    }
+  }
+  res.end("Bye Client!");
+});
+
+server.listen(port, hostName);
+
+console.log("grab market server on!");
