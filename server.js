8bb0b60e1fceadf55757f9c7edd0bc7e00a9fe3d
JaeWooYi <wodn0101@gmail.com>
2022-10-05 13:45:20 +0900

    I hate postman

--- server.js
+++ server.js
@@ -8,47 +8,64 @@ app.use(express.json());
 app.use(cors());
 
 app.get("/products", async (req, res) => {
-  const query = req.query;
-  console.log("QUERY : " + query);
-  res.send({
-    products: [
-      {
-        id: 1,
-        name: "농구공",
-        price: 100000,
-        seller: "조던",
-        imageUrl: "images/products/basketball1.jpeg",
-      },
-      {
-        id: 2,
-        name: "축구공",
-        price: 50000,
-        seller: "메시",
-        imageUrl: "images/products/soccerball1.jpg",
-      },
-      {
-        id: 3,
-        name: "키보드",
-        price: 10000,
-        seller: "그랩",
-        imageUrl: "images/products/keyboard1.jpg",
-      },
-    ],
-  });
+  models.Product.findAll({
+    order: [["createdAt", "DESC"]],
+    attributes: ["id", "name", "price", "createdAt", "seller", "imageUrl"],
+  })
+    .then(result => {
+      console.log("PRODUCTS : " + result);
+      res.send({
+        products: result,
+      });
+    })
+    .catch(error => {
+      console.error(error);
+      res.send("ERROR..");
+    });
 });
 
 app.post("/products", (req, res) => {
   const body = req.body;
-  // res.send("상품이 등록 되었습니다.");
-  res.send({
-    body,
-  });
+  const { name, description, price, seller } = body;
+  if (!name || !description || !price || !seller) {
+    res.send("Input Every Field.");
+  }
+  models.Product.create({
+    name,
+    description,
+    price,
+    seller,
+  })
+    .then(result => {
+      console.log("Product Create Result : " + result);
+      res.send({
+        result: result,
+      });
+    })
+    .catch(error => {
+      console.log(error);
+      res.send("Product Upload Fail..");
+    });
 });
 
-app.get("/products/:id/events/:eventID", (req, res) => {
+app.get("/products/:id", (req, res) => {
   const params = req.params;
   const { id, eventID } = params;
-  res.send(`ID : ${id}, eventID : ${eventID}`);
+  models.Product.findOne({
+    where: {
+      id: id,
+    },
+  })
+    .then(result => {
+      console.log("PRODUCT : " + result);
+      res.send({
+        product: result,
+      });
+    })
+    .catch(error => {
+      console.error(error);
+      res.send("Error..");
+    });
 });
 
 app.listen(port, () => {
